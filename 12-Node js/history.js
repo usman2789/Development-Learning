707d8bb0f7440400c599774e704bfc96caf7b8f9
usman2789 <mangoapple027@gmail.com>
2025-04-27 12:51:14 +0500

    .

---
+++ 12-Node js/history.js
@@ -0,0 +1,20 @@
+//                                  History of Node.js:
+
+//Node.js Creation:   Node.js was created in 2009 by Ryan Doll. It's a tool that allows JavaScript to run 
+// outside of a web browser, using Chrome's V8 engine.
+
+
+//npm Introduction:     In 2011, npm (node package manager) was introduced. It made it easy to share and
+//  manage Node.js code libraries.
+
+
+//Node.js Foundation:    In 2015, the Node.js Foundation was formed, including big companies like IBM,
+//  Microsoft, and PayPal. This helped guide the development and growth of Node.js.
+
+
+//Community Growth: Today, Node.js has a large, active community with many conferences and events worldwide.
+
+
+
+//This tool is now widely used across various industries for building web applications and controlling servers.
+
